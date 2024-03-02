233be69b9be786d6d71158b61b3e51451668b302
Ryan Cortes <ryancort@udel.edu>
2024-03-02 12:10:21 -0500

    selenium setup

--- testfileinvs.py
+++ testfileinvs.py
@@ -1,11 +1,5 @@
-print("test file")
+import os
+from selenium import webdriver
 
-print("Hello")
-
-print("goodbye")
-
-print("test")
-
-print("hello world")
-
-#https://apify.com/store/categories/ai
+os.environ['PATH'] += r"C:\Users\Ryan Cortes\Desktop\selenium test\chromedriver_win32 (1)"
+driver = webdriver.Chrome()
